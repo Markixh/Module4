1369d0a667c3b53e7e13c44174f79c87e8867dbf
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-22 19:28:48 +0300

    Добавлено задание 4.1.4

---
+++ Task4.1.4/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task4_1_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = "", B = ""; //Без присвоения значения не выполнял операцию
+            var C = A != B;
+            Console.WriteLine(C);
+            Console.ReadKey();
+        }
+    }
+}
