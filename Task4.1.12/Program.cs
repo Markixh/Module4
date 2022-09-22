a7994776ee5b27ae4e5234fb61aed5f55135dd53
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-22 21:26:30 +0300

    Добавлено задание 4.1.12

---
+++ Task4.1.12/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task4._1._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 6;
+            var b = 7;
+
+            if (a < b)
+            {
+                Console.WriteLine("Условие истинно");
+            }
+            else
+            {
+                Console.WriteLine("Условие ложно");
+            }
+        }
+    }
+}
