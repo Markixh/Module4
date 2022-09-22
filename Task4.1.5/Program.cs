8bcb8c97e7d41f3be6a025cbe48b3e3a90d56b82
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-22 19:34:24 +0300

    Добавлено задание 4.1.5

---
+++ Task4.1.5/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task4_1_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 0, B = 0; //Без присвоения значения не выполнял операцию
+            double X = 0 , Y = 0;
+            var C = A < B | X > Y;
+            Console.WriteLine(C);
+            Console.ReadKey();
+        }
+    }
+}
