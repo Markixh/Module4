df08ba00fdde1adf67c5d40090940b4828dac5a0
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-24 00:10:34 +0300

    Добавлено задание 4.2.11

---
+++ Task4.2.11/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+
+namespace Task4_2_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Цикл do...while");
+            int t = 0;
+            do 
+            {
+                Console.WriteLine(t);
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Your color is yellow!");
+                        break;
+                }
+                t++;
+            }
+            while (t < 3);
+            Console.ReadKey();
+        }
+    }
+}
