ed37d32e597618a1e8988aa0fe7e183ff691e6a0
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-24 13:20:21 +0300

    Добавлен ввод информации о возрасте пользователя и трех его любимых цветов (4.5.5)

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -7,13 +7,17 @@ namespace Module4
         static void Main(string[] args)
         {
             (string Name, string LastName, string Login, int LoginLength, bool IfPet, byte Age, string[] FavColors) User;
+
             Console.WriteLine("Введите имя");
             User.Name = Console.ReadLine();
+
             Console.WriteLine("Введите фамилию"); 
             User.LastName = Console.ReadLine();
+
             Console.WriteLine("Введите логин");
             User.Login = Console.ReadLine();
             User.LoginLength = User.Login.Length;
+
             Console.WriteLine("Есть ли у вас животные? Да или Нет");
             switch (Console.ReadLine())
             {
@@ -26,7 +30,17 @@ namespace Module4
                 default:
                     User.IfPet = false;
                     break;
-            }            
+            }
+
+            Console.WriteLine("Введите возраст пользователя");
+            User.Age = Convert.ToByte(Console.ReadLine());
+
+            User.FavColors = new string[3];
+            Console.WriteLine("Введите три любимых цвета пользователя");
+            for (int i = 0; i < User.FavColors.Length; i++)
+            {
+                User.FavColors[i] = Console.ReadLine();
+            }
         }
     }
 }
