49507f4fc18c5978410745d938ceb8e04d98873a
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-24 08:02:29 +0300

    Добавлено задание 4.3.7

---
+++ Task4.3.7/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task4_3_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string eman = "";
+            Console.WriteLine("Введите свое имя");
+            var name = Console.ReadLine();
+            Console.WriteLine("Ваше имя в обратном порядке:");
+            foreach (var ch in name)
+                eman = ch + eman;
+            Console.WriteLine(eman);
+            Console.ReadKey();
+        }
+    }
+}
