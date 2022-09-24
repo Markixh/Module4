3e9d3f1925fae773628f2fbc15a30938fdfe30c4
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-24 07:47:23 +0300

    Добавлено задание 4.3.4

---
+++ Task4.3.4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task4_3_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя");
+            var name = Console.ReadLine();
+            Console.WriteLine("Ваше имя по буквам:");
+            for (int i = 0; i < name.Length; i++)
+                Console.Write($"{name[i]} ");
+            Console.Write("Последняя буква вашего имени: {0}", name[name.Length - 1]);
+            Console.ReadKey();
+        }
+    }
+}
