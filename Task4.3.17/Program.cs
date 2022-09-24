9c2fc0e038b39f20373771a1d3bb06887ef0d07b
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-24 09:37:42 +0300

    Добавлено задание 4.3.17

---
+++ Task4.3.17/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task4_3_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            for (int k = 0; k <= arr.GetUpperBound(0); k++)
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                    for (int i = j; i <= arr.GetUpperBound(1) - 1; i++)
+                        if (arr[k, i + 1] < arr[k, i])
+                        {
+                            var temp = arr[k, i];
+                            arr[k, i] = arr[k, i + 1];
+                            arr[k, i + 1] = temp;
+                        }
+            for (int j = 0; j <= arr.GetUpperBound(0); j++)
+            {
+                for (int i = 0; i <= arr.GetUpperBound(1); i++)
+                    Console.Write(arr[j, i] + " ");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
