e5d228fb7669e468d4dc640bb6b9295f7135491e
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-24 09:54:20 +0300

    Добавлено задание 4.4.2

---
+++ Task4.4.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Linq;
+
+namespace Task4_4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, byte age) anketa;
+            Console.Write("Введите ваше Имя: ");
+            anketa.name = Console.ReadLine();
+            Console.Write("Введите ваш возраст: ");
+            anketa.age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine($"Ваше имя {anketa.name}, а возраст {anketa.age}");
+            Console.ReadKey();
+        }
+    }
+}
