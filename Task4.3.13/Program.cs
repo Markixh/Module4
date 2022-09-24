01e9dde7ec46a9b89887143708a8e40fe1be5d4b
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-24 08:57:04 +0300

    Добавлено задание 4.3.13

---
+++ Task4.3.13/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task4_3_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            int sum = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
