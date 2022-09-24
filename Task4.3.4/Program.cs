e1fcc13d92561190d4e738aad8dbdf6aed3d4acf
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-24 07:51:20 +0300

    Внесены изменения в задание 4.3.4, использован цикл foreach

--- Task4.3.4/Program.cs
+++ Task4.3.4/Program.cs
@@ -7,8 +7,8 @@
             Console.WriteLine("Введите свое имя");
             var name = Console.ReadLine();
             Console.WriteLine("Ваше имя по буквам:");
-            for (int i = 0; i < name.Length; i++)
-                Console.Write($"{name[i]} ");
+            foreach (var ch in name)
+                Console.Write($"{ch} ");
             Console.Write("Последняя буква вашего имени: {0}", name[name.Length - 1]);
             Console.ReadKey();
         }
