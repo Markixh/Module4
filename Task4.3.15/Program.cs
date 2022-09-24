2273b353e91df3c7400c98b9f87bfc8a6fe298f6
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-24 09:12:03 +0300

    Добавлено задание 4.3.15

---
+++ Task4.3.15/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task4_3_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] {4, -4, 2, -4, -2, 0, 5, 6, -8};
+            int count = 0;
+            for(int i = 0; i < arr.Length; i++)
+                if(arr[i] > 0)
+                    count++;
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
