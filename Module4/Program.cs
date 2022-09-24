34ad3211e84f403f5694ebdddbec7d58f45d84f7
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-24 14:09:19 +0300

    Добавлена возможность вносить информацию по трем пользователям

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -6,41 +6,45 @@ namespace Module4
     {
         static void Main(string[] args)
         {
-            (string Name, string LastName, string Login, int LoginLength, bool IfPet, byte Age, string[] FavColors) User;
-
-            Console.WriteLine("Введите имя");
-            User.Name = Console.ReadLine();
-
-            Console.WriteLine("Введите фамилию"); 
-            User.LastName = Console.ReadLine();
-
-            Console.WriteLine("Введите логин");
-            User.Login = Console.ReadLine();
-            User.LoginLength = User.Login.Length;
-
-            Console.WriteLine("Есть ли у вас животные? Да или Нет");
-            switch (Console.ReadLine())
+            (string Name, string LastName, string Login, int LoginLength, bool IfPet, byte Age, string[] FavColors)[] User = new(string, string, string, int, bool, byte, string[])[3];
+            for (int i = 0; i < User.Length; i++)
             {
-                case "Да":
-                case "да":
-                case "Yes":
-                case "yes":
-                    User.IfPet = true;
-                    break;
-                default:
-                    User.IfPet = false;
-                    break;
-            }
+                Console.WriteLine("Данные {0} пользователя", i + 1);
+                Console.WriteLine("Введите имя");
+                User[i].Name = Console.ReadLine();
 
-            Console.WriteLine("Введите возраст пользователя");
-            User.Age = Convert.ToByte(Console.ReadLine());
+                Console.WriteLine("Введите фамилию");
+                User[i].LastName = Console.ReadLine();
 
-            User.FavColors = new string[3];
-            Console.WriteLine("Введите три любимых цвета пользователя");
-            for (int i = 0; i < User.FavColors.Length; i++)
-            {
-                User.FavColors[i] = Console.ReadLine();
+                Console.WriteLine("Введите логин");
+                User[i].Login = Console.ReadLine();
+                User[i].LoginLength = User[i].Login.Length;
+
+                Console.WriteLine("Есть ли у вас животные? Да или Нет");
+                switch (Console.ReadLine())
+                {
+                    case "Да":
+                    case "да":
+                    case "Yes":
+                    case "yes":
+                        User[i].IfPet = true;
+                        break;
+                    default:
+                        User[i].IfPet = false;
+                        break;
+                }
+
+                Console.WriteLine("Введите возраст пользователя");
+                User[i].Age = Convert.ToByte(Console.ReadLine());
+
+                User[i].FavColors = new string[3];
+                Console.WriteLine("Введите три любимых цвета пользователя");
+                for (int j = 0; j < User[i].FavColors.Length; j++)
+                {
+                    User[i].FavColors[i] = Console.ReadLine();
+                }
             }
+            Console.ReadKey();
         }
     }
 }
