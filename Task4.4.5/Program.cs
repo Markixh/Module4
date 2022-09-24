d9d5f3dcb18bb9afc73eabcdb1f0d329362df060
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-24 10:16:47 +0300

    Добавлено задание 4.4.5

---
+++ Task4.4.5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Linq;
+
+namespace Task4._4._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string Type, double Age, int NameCount) Pet;
+            Console.Write("Введите имя вашего питомца: ");
+            Pet.Name = Console.ReadLine();
+            Pet.NameCount = Pet.Name.Length;
+            Console.Write("Какой у вас питомец: ");
+            Pet.Type = Console.ReadLine();
+            Console.Write("Введите возраст вашего питомца: ");
+            Pet.Age = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"У вас {Pet.Type} {Pet.Name}, возраст {Pet.Age.ToString("0.0")}");
+            Console.ReadKey();
+        }
+    }
+}
