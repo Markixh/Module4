7ec3569f382ae3de66018ab6e51b85545fa7a3ea
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-24 09:22:41 +0300

    Добавлено задание 4.3.16

---
+++ Task4.3.16/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task4_3_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            int count = 0;
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                    if (arr[i, j] > 0)
+                        count++;
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
