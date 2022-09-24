04dc011c5499729ee7704fbc0d15f3ca5805df96
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-24 08:43:50 +0300

    Добавлено задание 4.3.12, сортировка с нахождением минимального на каждой итерации

---
+++ Task4.3.12/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task4_3_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            int[] sortarr = new int[arr.Length];
+            int min = 100000;
+            int prev = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if ((arr[j] < min) && (arr[j] > prev))
+                    {
+                        min = arr[j];
+                    }
+                }
+                sortarr[i] = min;
+                prev = min;
+                min = 100000;
+            }
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(sortarr[i]);
+            Console.ReadKey();
+        }
+    }
+}
