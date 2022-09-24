475793a70f6a7b5fdbbb65b77d607af3bc328d3e
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-24 13:02:24 +0300

    Заполнение кортежа для Имени, Фамилии и логина (4.5.2)

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             (string Name, string LastName, string Login, int LoginLength, bool IfPet, byte Age, string[] FavColors) User;
+            Console.WriteLine("Введите имя");
+            User.Name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию"); 
+            User.LastName = Console.ReadLine();
+            Console.WriteLine("Введите логин");
+            User.Login = Console.ReadLine();
         }
     }
 }
